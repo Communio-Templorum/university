cfaa0c45ca1b997c30f71d049edd7d45dc197561
Sam Grundman <samuel@yodas.ws>
2021-10-11 11:44:46 -0400

    Add page for Registrar's Office
    blank for now, obviously

---
+++ src/pages/registrar/ctrl.js
@@ -0,0 +1,7 @@
+yodasws.page('pageRegistrar').setRoute({
+	title: "Registrar's Office",
+	template: 'pages/registrar/registrar.html',
+	canonicalRoute: '/registrar/',
+	route: '/registrar/?',
+}).on('load', () => {
+});
