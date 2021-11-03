91dfbe98f48b669869e9e87a817a8c3682afd20a
Sam Grundman <samuel@yodas.ws>
2021-11-03 16:06:04 -0400

    Add a smidge more flare to Admissions page

--- src/pages/admissions/ctrl.js
+++ src/pages/admissions/ctrl.js
@@ -1,7 +1,16 @@
 yodasws.page('pageAdmissions').setRoute({
 	title: 'Admissions',
-	template: 'pages/admissions/admissions.html',
 	canonicalRoute: '/admissions/',
-	route: '/admissions/?',
+	template(match, ...p) {
+		const path = p.join('/').replace(/\/+/g, '/').replace(/^\/|\/$/g, '').split('/').filter(p => p != '');
+		if (path.length === 0) {
+			return 'pages/admissions/admissions.html';
+		}
+		return {
+			canonicalRoute: '/admissions/' + path.join('/') + '/',
+			template: 'pages/admissions/' + path.join('.') + '.html',
+		};
+	},
+	route: '/admissions(/.*)*',
 }).on('load', () => {
 });
